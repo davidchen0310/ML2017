601eec3a8fbf92f34cfc5eaa54ca947693554c9a
晏祐 張 <b03705028@ntu.edu.tw>
2017-06-07 22:35:07 +0800

    specify python version in scripts

--- hw6/hw6_best.sh
+++ hw6/hw6_best.sh
@@ -1,1 +1,1 @@
-python predict.py --base-folder $1 --res-file $2 --model-weights-path DNN_normalize_85689.h5 --use-DNN --normalize 
+python3.5 predict.py --base-folder $1 --res-file $2 --model-weights-path DNN_normalize_85689.h5 --use-DNN --normalize 
